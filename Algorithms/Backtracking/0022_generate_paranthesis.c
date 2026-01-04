ba52d5a2546a6761496883b1af702de2295aecae
max <141556361+maxi-cmyk@users.noreply.github.com>
2026-01-04 18:18:12 +0000

    leetcode to cccccc

--- Algorithms/Backtracking/0022_generate_paranthesis.c
+++ Algorithms/Backtracking/0022_generate_paranthesis.c
@@ -3,7 +3,51 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void backtrack(char **res, int *returnSize, char *temp, int n, int open, int close, int pos){
+    // (())-> ()(), so we are just updating the inner few, need all combinations 
+    if (pos == 2*n){ //buffer
+        temp[pos] = '\0';
+        res[*returnSize] = (char*)malloc((2*n+1) *sizeof(char));
+        strcpy(res[*returnSize], temp);
+        (*returnSize)++;
+        return;
+    }
+    
+
+    //open bracket valid? 
+    if (open < n){
+        temp[pos] = '('; //((
+        backtrack(res, returnSize, temp, n, open+1, close, pos+1); //updates here 
+    }
+    if (close < open){
+        temp[pos] = ')'; //  ))
+        backtrack(res, returnSize, temp, n, open, close+1, pos+1); //updates here 
+    }
+}
+
+char **generateParenthesis (int n, int *returnSize){
+    char **res = (char**)malloc(2000 *sizeof(char*));
+    //buffer 
+    char *temp = (char*)malloc((2*n+1) * sizeof(char));
+    *returnSize = 0;
+    backtrack(res, returnSize, temp, n, 0, 0, 0);
+    free(temp);
+    return res;
+}
 
 int main() {
+    int n = 3;
+    int returnSize;
+    char **result = generateParenthesis(n, &returnSize);
+
+    printf("Generated %d pairs for n=%d:\n", returnSize, n);
+    for (int i = 0; i < returnSize; i++) {
+        printf("%s\n", result[i]);
+        free(result[i]); // Clean up each string
+    }
+    free(result); // Clean up the array of pointers
+
     return 0;
 }
--- Algorithms/Two-Pointers/0031_next_permutations.c
+++ Algorithms/Two-Pointers/0031_next_permutations.c
@@ -4,6 +4,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//swap helper function
+void swap (int *a, int *b){
+    int temp = *a; //swapping addresses
+    *a = *b;
+    *b = temp;
+}
+
+void reverse (int *nums, int start, int end){
+    while (start < end) {
+        swap(&nums[start],&nums[end]); //swap addresses
+        start++;
+        end--;
+    }
+}
+
+void nextPermutation (int *nums, int numsSize){
+    int i = numsSize - 1;
+    while (i > 0 && nums[i-1] >= nums[i]){ //find pivot, first decreasing element
+        i--;
+    }
+    if (i == 0){
+        reverse(nums, i, numsSize-1);
+        return;
+    }
+    int j = numsSize - 1;
+    //looking for smallest element from right of pivot that is larger than pivot 
+    while (j > i-1 && nums[j] <= nums[i-1]){ 
+        j--;
+
+    } 
+    swap(&nums[i-1], &nums[j]);
+    reverse(nums, i, numsSize-1);
+}
+
+void printArray(int *nums, int size) {
+    printf("[");
+    for (int i = 0; i < size; i++) {
+        printf("%d%s", nums[i], i == size - 1 ? "" : ", ");
+    }
+    printf("]\n");
+}
+
 int main() {
+    // Case 1: Standard
+    int case1[] = {1, 2, 3};
+    nextPermutation(case1, 3);
+    printf("Case 1: "); printArray(case1, 3);
+
+    // Case 2: Wrap around
+    int case2[] = {3, 2, 1};
+    nextPermutation(case2, 3);
+    printf("Case 2: "); printArray(case2, 3);
+
+    // Case 3: Pivot at start
+    int case3[] = {1, 3, 2};
+    nextPermutation(case3, 3);
+    printf("Case 3: "); printArray(case3, 3);
+
     return 0;
 }
