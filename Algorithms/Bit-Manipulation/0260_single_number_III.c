6a5e8e662865cdcfb918d25cca9c9d93b55ca18c
maxi-cmyk <maxleong32@gmail.com>
2026-02-02 09:17:27 +0800

    single num 3

--- Algorithms/Bit-Manipulation/0260_single_number_III.c
+++ Algorithms/Bit-Manipulation/0260_single_number_III.c
@@ -4,6 +4,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    return 0;
+int* singleNumber(int* nums, int numsSize, int* returnSize){
+    //use XOR 
+    unsigned int xor = 0;
+    for (int i = 0; i < numsSize; i++){
+        xor ^= nums[i];
+    }
+    unsigned int diff_bit = xor & -(unsigned int)xor;
+    //partition 
+    int a = 0;
+    int b = 0;
+    for (int i = 0; i < numsSize; i++){
+        if (nums[i] & diff_bit){
+            a^= nums[i];
+        } else {
+            b ^= nums[i];
+        }
+    }
+    int* result = (int*)malloc(2 * sizeof(int));
+    result[0] = a;
+    result[1] = b;
+
+    *returnSize = 2;
+    return result;
 }
