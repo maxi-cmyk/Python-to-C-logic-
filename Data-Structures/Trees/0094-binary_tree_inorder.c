9e9b589970c5df73df31f29fe7dee7ada903a9b7
maxi-cmyk <maxleong32@gmail.com>
2026-02-07 15:03:47 +0800

    inorder traverse in c

--- Data-Structures/Trees/0094-binary_tree_inorder.c
+++ Data-Structures/Trees/0094-binary_tree_inorder.c
@@ -4,6 +4,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    return 0;
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+void traverse(struct TreeNode* root, int* res, int* idx){
+    //use *idx to get a count of how many values 
+    if (root == NULL){
+        return;
+    }
+    //go left
+    traverse(root->left, res, idx);
+    //go root
+    res[(*idx)++] = root->val;
+    //go right
+    traverse(root->right, res, idx);
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* res = (int*)malloc(100 * sizeof(int));
+    *returnSize = 0;
+    
+    traverse(root, res, returnSize);
+    
+    return res;
 }
