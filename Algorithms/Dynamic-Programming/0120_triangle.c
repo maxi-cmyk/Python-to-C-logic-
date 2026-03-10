451a0ba14ff53d64213f5fbb0a001ae62dcaef31
maxi-cmyk <maxleong32@gmail.com>
2026-03-10 10:50:24 +0800

    120

--- Algorithms/Dynamic-Programming/0120_triangle.c
+++ Algorithms/Dynamic-Programming/0120_triangle.c
@@ -4,6 +4,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    return 0;
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+
+int minimumTotal(int** triangle, int triangleSize, int* triangleColSize){
+    int* dp = (int*)malloc(triangleSize * sizeof(int));
+
+    for (int i = 0; i < triangleSize; i++){
+        dp[i] = triangle[triangleSize - 1][i];
+    }
+
+    //bottom up
+    for (int row = triangleSize - 2; row >= 0; row--){
+        for (int i = 0; i <= row; i++){
+            dp[i] = triangle[row][i] + MIN(dp[i], dp[i+1]);
+        }
+    }
+    int result = dp[0];
+    free(dp);
+    return result;
 }
+
