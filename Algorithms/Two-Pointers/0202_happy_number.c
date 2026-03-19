5bd3e3c839b62d5768ac729a7e5e6abfd9ac2ad0
maxi-cmyk <maxleong32@gmail.com>
2026-03-19 13:54:14 +0800

    happy in c

--- Algorithms/Two-Pointers/0202_happy_number.c
+++ Algorithms/Two-Pointers/0202_happy_number.c
@@ -3,7 +3,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h> 
 
-int main() {
-    return 0;
+//floyd's cycle detection algo (tortise and hare algo)
+
+int nextNum (int n){
+    int output = 0; 
+
+    while (n != 0){
+        int digit = n % 10; 
+        //remember that c has no ** function
+        output += digit * digit;
+        n /= 10;
+    }
+    return output;
+}
+
+bool isHappy(int n){
+    int slow = nextNum(n);
+    int fast = nextNum(nextNum(n));
+
+    while (slow != fast){
+        if (fast == 1){
+            return true;
+        }
+        slow = nextNum(slow);
+        fast = nextNum(nextNum(fast));
+
+    }
+    return slow == 1;
 }
