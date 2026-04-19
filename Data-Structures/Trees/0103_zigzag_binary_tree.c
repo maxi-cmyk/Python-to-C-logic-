f1740eb13addf014d2c9ca07e3df6a7648c8bdf5
maxi-cmyk <maxleong32@gmail.com>
2026-04-19 15:29:32 +0800

    wtf

--- Data-Structures/Trees/0103_zigzag_binary_tree.c
+++ Data-Structures/Trees/0103_zigzag_binary_tree.c
@@ -3,7 +3,50 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h> 
 
-int main() {
-    return 0;
+struct TreeNode {
+    int val; 
+    struct TreeNode* left; 
+    struct TreeNode* right;
+};
+
+int* zigzagLevelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes){
+    if (root == NULL){
+        *returnSize = 0; 
+        *returnColumnSizes = 0; 
+        return NULL;
+    }
+
+    int** res = (int**)malloc(2000 * sizeof(int*)); 
+    *returnColumnSizes = (int*)malloc(2000 * sizeof(int)); 
+    *returnSize = 0; 
+
+    struct TreeNode* queue[2000];
+    int head = 0; 
+    int tail = 0; 
+
+    //place root here first
+    queue[tail++] = root;
+    int left_to_right = 1;
+
+    while (head < tail){ 
+        int levelSize = tail - head; 
+        int* currentLevel = (int*)malloc(levelSize * sizeof(int)); 
+        //col is top to bottom, row is left to right
+        (*returnColumnSizes)[*returnSize] = levelSize;
+
+        for (int i = 0; i < levelSize; i++){
+            struct TreeNode* node = queue[head++];
+            int index = left_to_right ? i : (levelSize - 1 - i);
+            currentLevel[index] = node->val;
+
+            if(node->left != NULL) queue[tail++] = node->left;
+            if(node->right != NULL) queue[tail++] = node->right;
+        }
+        res[*returnSize] = currentLevel; 
+        (*returnSize)++;
+        left_to_right = !left_to_right;
+    }
+    return res;
 }
